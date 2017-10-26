d333617d37228279a8b4d45f338c142d311a4e44
Liu Chong <mail@clojure.cn>
2017-10-26 15:36:27 +0800

    Add the very first version.

---
+++ gatsby-config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  siteMetadata: {
+    siteName: 'Gatsby starter blog typescript'
+  },
+  plugins: ['gatsby-plugin-typescript']
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react'
+
+export default function index() {
+  return (
+    <div>
+      <h1>Gatsby starter blog typescript</h1>
+    </div>
+  )
+}
