1f0aaa5e8030ee00a1c3fb83a85e4e9017e9c72c
Liu Chong <mail@clojure.cn>
2017-11-01 23:42:00 +0800

    Add markdown support.

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,22 @@ module.exports = {
   siteMetadata: {
     siteName: 'gatsby starter blog typescript'
   },
-  plugins: ['gatsby-plugin-typescript']
+  plugins: [
+    'gatsby-plugin-typescript',
+    'gatsby-plugin-catch-links',
+    'gatsby-plugin-react-helmet',
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/src/pages`,
+        name: 'pages'
+      }
+    },
+    {
+      resolve: 'gatsby-transformer-remark',
+      options: {
+        plugins: []
+      }
+    }
+  ]
 }
